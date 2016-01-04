921f85c19fb0135b60867476b085631a1b071546
tdoric <tdoric8@gmail.com>
2016-01-04 18:35:47 +0100

    RSA algorithm
    Rsa algoritam i digitalni potpis izradeni za potrebe kolegija

---
+++ Operacijski sustavi 2/frmAsimetricnoRSA.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace Operacijski_sustavi_2
+{
+    partial class frmAsimetricnoRSA
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAsimetricnoRSA));
+            this.grpDekriptiranjeRSA = new System.Windows.Forms.GroupBox();
+            this.lblDekriptiraniRSA = new System.Windows.Forms.Label();
+            this.txtDekriptiraniRSA = new System.Windows.Forms.TextBox();
+            this.btnDekriptirajRSA = new System.Windows.Forms.Button();
+            this.txtDatotekaDekriptRSA = new System.Windows.Forms.TextBox();
+            this.lblDatotekaDekriptRSA = new System.Windows.Forms.Label();
+            this.lblPutanjaDekriptRSA = new System.Windows.Forms.Label();
+            this.txtPutanjaDekriptRSA = new System.Windows.Forms.TextBox();
+            this.btnOpenFileDekriptRSA = new System.Windows.Forms.Button();
+            this.grpKriptiranjeRSA = new System.Windows.Forms.GroupBox();
+            this.lblKriptiraniRSA = new System.Windows.Forms.Label();
+            this.txtKriptiraniRSA = new System.Windows.Forms.TextBox();
+            this.btnKriptirajRSA = new System.Windows.Forms.Button();
+            this.txtDatotekaRSA = new System.Windows.Forms.TextBox();
+            this.lblDatotekaRSA = new System.Windows.Forms.Label();
+            this.lblPutanjaRSA = new System.Windows.Forms.Label();
+            this.txtPutanjaRSA = new System.Windows.Forms.TextBox();
+            this.btnOpenFileRSA = new System.Windows.Forms.Button();
+            this.btnKljucRSA = new System.Windows.Forms.Button();
+            this.openFileDialogRSAKript = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialogRSADekript = new System.Windows.Forms.OpenFileDialog();
+            this.grpDekriptiranjeRSA.SuspendLayout();
+            this.grpKriptiranjeRSA.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpDekriptiranjeRSA
+            // 
+            this.grpDekriptiranjeRSA.Controls.Add(this.lblDekriptiraniRSA);
+            this.grpDekriptiranjeRSA.Controls.Add(this.txtDekriptiraniRSA);
+            this.grpDekriptiranjeRSA.Controls.Add(this.btnDekriptirajRSA);
+            this.grpDekriptiranjeRSA.Controls.Add(this.txtDatotekaDekriptRSA);
+            this.grpDekriptiranjeRSA.Controls.Add(this.lblDatotekaDekriptRSA);
+            this.grpDekriptiranjeRSA.Controls.Add(this.lblPutanjaDekriptRSA);
+            this.grpDekriptiranjeRSA.Controls.Add(this.txtPutanjaDekriptRSA);
+            this.grpDekriptiranjeRSA.Controls.Add(this.btnOpenFileDekriptRSA);
+            this.grpDekriptiranjeRSA.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.grpDekriptiranjeRSA.Location = new System.Drawing.Point(12, 282);
+            this.grpDekriptiranjeRSA.Name = "grpDekriptiranjeRSA";
+            this.grpDekriptiranjeRSA.Size = new System.Drawing.Size(590, 220);
+            this.grpDekriptiranjeRSA.TabIndex = 22;
+            this.grpDekriptiranjeRSA.TabStop = false;
+            this.grpDekriptiranjeRSA.Text = "Dekriptiranje";
+            // 
+            // lblDekriptiraniRSA
+            // 
+            this.lblDekriptiraniRSA.AutoSize = true;
+            this.lblDekriptiraniRSA.Location = new System.Drawing.Point(28, 120);
+            this.lblDekriptiraniRSA.Name = "lblDekriptiraniRSA";
+            this.lblDekriptiraniRSA.Size = new System.Drawing.Size(89, 13);
+            this.lblDekriptiraniRSA.TabIndex = 17;
+            this.lblDekriptiraniRSA.Text = "Dekriptirani tekst:";
+            // 
+            // txtDekriptiraniRSA
+            // 
+            this.txtDekriptiraniRSA.Location = new System.Drawing.Point(26, 145);
+            this.txtDekriptiraniRSA.Multiline = true;
+            this.txtDekriptiraniRSA.Name = "txtDekriptiraniRSA";
+            this.txtDekriptiraniRSA.ReadOnly = true;
+            this.txtDekriptiraniRSA.Size = new System.Drawing.Size(414, 69);
+            this.txtDekriptiraniRSA.TabIndex = 16;
+            // 
+            // btnDekriptirajRSA
+            // 
+            this.btnDekriptirajRSA.Location = new System.Drawing.Point(458, 145);
+            this.btnDekriptirajRSA.Name = "btnDekriptirajRSA";
+            this.btnDekriptirajRSA.Size = new System.Drawing.Size(114, 69);
+            this.btnDekriptirajRSA.TabIndex = 15;
+            this.btnDekriptirajRSA.Text = "Dekriptiraj";
+            this.btnDekriptirajRSA.UseVisualStyleBackColor = true;
+            this.btnDekriptirajRSA.Click += new System.EventHandler(this.btnDekriptirajRSA_Click);
+            // 
+            // txtDatotekaDekriptRSA
+            // 
+            this.txtDatotekaDekriptRSA.Location = new System.Drawing.Point(26, 81);
+            this.txtDatotekaDekriptRSA.Name = "txtDatotekaDekriptRSA";
+            this.txtDatotekaDekriptRSA.ReadOnly = true;
+            this.txtDatotekaDekriptRSA.Size = new System.Drawing.Size(414, 20);
+            this.txtDatotekaDekriptRSA.TabIndex = 14;
+            // 
+            // lblDatotekaDekriptRSA
+            // 
+            this.lblDatotekaDekriptRSA.AutoSize = true;
+            this.lblDatotekaDekriptRSA.Location = new System.Drawing.Point(28, 65);
+            this.lblDatotekaDekriptRSA.Name = "lblDatotekaDekriptRSA";
+            this.lblDatotekaDekriptRSA.Size = new System.Drawing.Size(54, 13);
+            this.lblDatotekaDekriptRSA.TabIndex = 13;
+            this.lblDatotekaDekriptRSA.Text = "Datoteka:";
+            // 
+            // lblPutanjaDekriptRSA
+            // 
+            this.lblPutanjaDekriptRSA.AutoSize = true;
+            this.lblPutanjaDekriptRSA.Location = new System.Drawing.Point(26, 16);
+            this.lblPutanjaDekriptRSA.Name = "lblPutanjaDekriptRSA";
+            this.lblPutanjaDekriptRSA.Size = new System.Drawing.Size(91, 13);
+            this.lblPutanjaDekriptRSA.TabIndex = 12;
+            this.lblPutanjaDekriptRSA.Text = "Putanja datoteke:";
+            // 
+            // txtPutanjaDekriptRSA
+            // 
+            this.txtPutanjaDekriptRSA.Location = new System.Drawing.Point(26, 32);
+            this.txtPutanjaDekriptRSA.Name = "txtPutanjaDekriptRSA";
+            this.txtPutanjaDekriptRSA.Size = new System.Drawing.Size(414, 20);
+            this.txtPutanjaDekriptRSA.TabIndex = 11;
+            // 
+            // btnOpenFileDekriptRSA
+            // 
+            this.btnOpenFileDekriptRSA.Location = new System.Drawing.Point(458, 32);
+            this.btnOpenFileDekriptRSA.Name = "btnOpenFileDekriptRSA";
+            this.btnOpenFileDekriptRSA.Size = new System.Drawing.Size(114, 23);
+            this.btnOpenFileDekriptRSA.TabIndex = 10;
+            this.btnOpenFileDekriptRSA.Text = "Odaberite datoteku";
+            this.btnOpenFileDekriptRSA.UseVisualStyleBackColor = true;
+            this.btnOpenFileDekriptRSA.Click += new System.EventHandler(this.btnOpenFileDekriptRSA_Click);
+            // 
+            // grpKriptiranjeRSA
+            // 
+            this.grpKriptiranjeRSA.Controls.Add(this.lblKriptiraniRSA);
+            this.grpKriptiranjeRSA.Controls.Add(this.txtKriptiraniRSA);
+            this.grpKriptiranjeRSA.Controls.Add(this.btnKriptirajRSA);
+            this.grpKriptiranjeRSA.Controls.Add(this.txtDatotekaRSA);
+            this.grpKriptiranjeRSA.Controls.Add(this.lblDatotekaRSA);
+            this.grpKriptiranjeRSA.Controls.Add(this.lblPutanjaRSA);
+            this.grpKriptiranjeRSA.Controls.Add(this.txtPutanjaRSA);
+            this.grpKriptiranjeRSA.Controls.Add(this.btnOpenFileRSA);
+            this.grpKriptiranjeRSA.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.grpKriptiranjeRSA.Location = new System.Drawing.Point(12, 41);
+            this.grpKriptiranjeRSA.Name = "grpKriptiranjeRSA";
+            this.grpKriptiranjeRSA.Size = new System.Drawing.Size(590, 220);
+            this.grpKriptiranjeRSA.TabIndex = 21;
+            this.grpKriptiranjeRSA.TabStop = false;
+            this.grpKriptiranjeRSA.Text = "Kriptiranje";
+            // 
+            // lblKriptiraniRSA
+            // 
+            this.lblKriptiraniRSA.AutoSize = true;
+            this.lblKriptiraniRSA.Location = new System.Drawing.Point(28, 112);
+            this.lblKriptiraniRSA.Name = "lblKriptiraniRSA";
+            this.lblKriptiraniRSA.Size = new System.Drawing.Size(76, 13);
+            this.lblKriptiraniRSA.TabIndex = 17;
+            this.lblKriptiraniRSA.Text = "Kriptirani tekst:";
+            // 
+            // txtKriptiraniRSA
+            // 
+            this.txtKriptiraniRSA.Location = new System.Drawing.Point(31, 128);
+            this.txtKriptiraniRSA.Multiline = true;
+            this.txtKriptiraniRSA.Name = "txtKriptiraniRSA";
+            this.txtKriptiraniRSA.ReadOnly = true;
+            this.txtKriptiraniRSA.Size = new System.Drawing.Size(414, 86);
+            this.txtKriptiraniRSA.TabIndex = 16;
+            // 
+            // btnKriptirajRSA
+            // 
+            this.btnKriptirajRSA.Location = new System.Drawing.Point(458, 128);
+            this.btnKriptirajRSA.Name = "btnKriptirajRSA";
+            this.btnKriptirajRSA.Size = new System.Drawing.Size(114, 86);
+            this.btnKriptirajRSA.TabIndex = 15;
+            this.btnKriptirajRSA.Text = "Kriptiraj";
+            this.btnKriptirajRSA.UseVisualStyleBackColor = true;
+            this.btnKriptirajRSA.Click += new System.EventHandler(this.btnKriptirajRSA_Click);
+            // 
+            // txtDatotekaRSA
+            // 
+            this.txtDatotekaRSA.Location = new System.Drawing.Point(31, 71);
+            this.txtDatotekaRSA.Name = "txtDatotekaRSA";
+            this.txtDatotekaRSA.ReadOnly = true;
+            this.txtDatotekaRSA.Size = new System.Drawing.Size(414, 20);
+            this.txtDatotekaRSA.TabIndex = 14;
+            // 
+            // lblDatotekaRSA
+            // 
+            this.lblDatotekaRSA.AutoSize = true;
+            this.lblDatotekaRSA.Location = new System.Drawing.Point(26, 55);
+            this.lblDatotekaRSA.Name = "lblDatotekaRSA";
+            this.lblDatotekaRSA.Size = new System.Drawing.Size(54, 13);
+            this.lblDatotekaRSA.TabIndex = 13;
+            this.lblDatotekaRSA.Text = "Datoteka:";
+            // 
+            // lblPutanjaRSA
+            // 
+            this.lblPutanjaRSA.AutoSize = true;
+            this.lblPutanjaRSA.Location = new System.Drawing.Point(26, 16);
+            this.lblPutanjaRSA.Name = "lblPutanjaRSA";
+            this.lblPutanjaRSA.Size = new System.Drawing.Size(91, 13);
+            this.lblPutanjaRSA.TabIndex = 12;
+            this.lblPutanjaRSA.Text = "Putanja datoteke:";
+            // 
+            // txtPutanjaRSA
+            // 
+            this.txtPutanjaRSA.Location = new System.Drawing.Point(31, 32);
+            this.txtPutanjaRSA.Name = "txtPutanjaRSA";
+            this.txtPutanjaRSA.Size = new System.Drawing.Size(414, 20);
+            this.txtPutanjaRSA.TabIndex = 11;
+            // 
+            // btnOpenFileRSA
+            // 
+            this.btnOpenFileRSA.Location = new System.Drawing.Point(458, 32);
+            this.btnOpenFileRSA.Name = "btnOpenFileRSA";
+            this.btnOpenFileRSA.Size = new System.Drawing.Size(114, 23);
+            this.btnOpenFileRSA.TabIndex = 10;
+            this.btnOpenFileRSA.Text = "Odaberite datoteku";
+            this.btnOpenFileRSA.UseVisualStyleBackColor = true;
+            this.btnOpenFileRSA.Click += new System.EventHandler(this.btnOpenFileRSA_Click);
+            // 
+            // btnKljucRSA
+            // 
+            this.btnKljucRSA.Location = new System.Drawing.Point(12, 12);
+            this.btnKljucRSA.Name = "btnKljucRSA";
+            this.btnKljucRSA.Size = new System.Drawing.Size(104, 23);
+            this.btnKljucRSA.TabIndex = 20;
+            this.btnKljucRSA.Text = "Generiraj ključeve";
+            this.btnKljucRSA.UseVisualStyleBackColor = true;
+            this.btnKljucRSA.Click += new System.EventHandler(this.btnKljucRSA_Click);
+            // 
+            // openFileDialogRSAKript
+            // 
+            this.openFileDialogRSAKript.Title = "Datoteka za kriptiranje";
+            // 
+            // openFileDialogRSADekript
+            // 
+            this.openFileDialogRSADekript.Title = "Datoteka za dekriptiranje";
+            // 
+            // frmAsimetricnoRSA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 537);
+            this.Controls.Add(this.grpDekriptiranjeRSA);
+            this.Controls.Add(this.grpKriptiranjeRSA);
+            this.Controls.Add(this.btnKljucRSA);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmAsimetricnoRSA";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Asimetrično - RSA";
+            this.grpDekriptiranjeRSA.ResumeLayout(false);
+            this.grpDekriptiranjeRSA.PerformLayout();
+            this.grpKriptiranjeRSA.ResumeLayout(false);
+            this.grpKriptiranjeRSA.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpDekriptiranjeRSA;
+        private System.Windows.Forms.Label lblDekriptiraniRSA;
+        private System.Windows.Forms.TextBox txtDekriptiraniRSA;
+        private System.Windows.Forms.Button btnDekriptirajRSA;
+        private System.Windows.Forms.TextBox txtDatotekaDekriptRSA;
+        private System.Windows.Forms.Label lblDatotekaDekriptRSA;
+        private System.Windows.Forms.Label lblPutanjaDekriptRSA;
+        private System.Windows.Forms.TextBox txtPutanjaDekriptRSA;
+        private System.Windows.Forms.Button btnOpenFileDekriptRSA;
+        private System.Windows.Forms.GroupBox grpKriptiranjeRSA;
+        private System.Windows.Forms.Label lblKriptiraniRSA;
+        private System.Windows.Forms.TextBox txtKriptiraniRSA;
+        private System.Windows.Forms.Button btnKriptirajRSA;
+        private System.Windows.Forms.TextBox txtDatotekaRSA;
+        private System.Windows.Forms.Label lblDatotekaRSA;
+        private System.Windows.Forms.Label lblPutanjaRSA;
+        private System.Windows.Forms.TextBox txtPutanjaRSA;
+        private System.Windows.Forms.Button btnOpenFileRSA;
+        private System.Windows.Forms.Button btnKljucRSA;
+        private System.Windows.Forms.OpenFileDialog openFileDialogRSAKript;
+        private System.Windows.Forms.OpenFileDialog openFileDialogRSADekript;
+    }
+}
